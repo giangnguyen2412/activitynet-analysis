82a40ba3156b5bacf17ec3f91145bd81b08db0ab
Giang Nguyen <hust2412@gmail.com>
2019-07-18 03:44:34 -0400

    Detect wrong annotations

--- captions/read_data.py
+++ captions/read_data.py
@@ -27,7 +27,10 @@ def read_data(path):
             sub_obj_num = len(obj['timestamps']) # Number of cuts in a video
             for i in range(sub_obj_num):
                 sub_obj_key = key + str(i)
-                prep_dict[sub_obj_key] = obj['timestamps'][i][1] - obj['timestamps'][i][0]
+                prep_dict[sub_obj_key] = obj['timestamps'][i][1] - obj['timestamps'][i][0]  # stop_time- start_time
+                if prep_dict[sub_obj_key] <= 0:
+                    print("In video of ID {}, the {}th cut has wrong annotations when start time is {} and "
+                          "stop time is {}".format(key, (i+1), obj['timestamps'][i][0], obj['timestamps'][i][1]))
 
         data_df = pd.DataFrame.from_dict(prep_dict, orient='index')
         df = pd.concat([df, data_df], axis=0, sort=True)
@@ -37,11 +40,11 @@ def read_data(path):
 
 df = read_data('train.json')
 df.dropna(inplace=True)
-print(df)
+#print(df)
 
 percentiles = [.10, .20, .30, .40, .50, .60, .70, .80, .90]
 include = ['float', 'int', 'object']
 desc = df.describe(percentiles=percentiles, include=include)
-print(desc)
+#print(desc)
 
 print("To debug above")
